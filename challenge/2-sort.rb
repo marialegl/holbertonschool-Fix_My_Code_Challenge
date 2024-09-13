f4917912e6302c0099919764923b3e4fe52e0681
marialegl <psico.marialegl@gmail.com>
2024-09-12 23:55:07 -0500

    Handling of numerical or numerical data

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -4,28 +4,22 @@
 #
 ###
 
-result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
-    i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
+numeric_args = []
+string_args = []
+
+
+ARGV.each do |arg|
+    if arg =~ /^-?[0-9]+$/
+        numeric_args << arg.to_i
+    else
+        string_args << arg
     end
-    result << i_arg if !is_inserted
 end
+
+sorted_numeric = numeric_args.sort
+sorted_strings = string_args.sort
+
+result = sorted_numeric + sorted_strings
 
 puts result
