472843a168e546d213470d9f60dc67bc512bec65
marialegl <psico.marialegl@gmail.com>
2024-09-15 02:28:56 -0500

    Remove nodes from doubly linked list

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,38 +11,42 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp = *head;
-	unsigned int p = 0;
+    dlistint_t *current = *head;
+    unsigned int i = 0;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	
-	while (tmp != NULL && p < index)
-	{
-		tmp = tmp->next;
-		p++;
-	}
-	if (tmp == NULL)
-	{
-		return (-1);
-	}
-	if (index == 0)
-	{
-		*head = tmp->next;
-		if (*head != NULL)
-		{
-			(*head)->prev = NULL;
-		}
-	}
-	else
-	{
-		tmp->prev->prev = (tmp)->prev;
-		if (tmp->next != NULL)
-			tmp->next->prev = tmp->prev;
-	}
+    if (*head == NULL)
+        return (-1);
 
-	free(tmp);
-	return (1);
+
+    while (current != NULL && i < index)
+    {
+        current = current->next;
+        i++;
+    }
+
+    if (current == NULL)
+        return (-1);
+
+
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        if (current->prev != NULL)
+            current->prev->next = current->next;
+        if (current->next != NULL)
+            current->next->prev = current->prev;
+    }
+
+    free(current);
+
+
+    if (*head == NULL)
+        return (1);
+
+    return (1);
 }
